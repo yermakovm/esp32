a249dd7f89cd032ab736dc9b74b0bb49b2df2a55
Mykhailo Yermakov <yermakovm81@gmail.com>
2026-06-01 19:56:29 +0300

    test and fix clangd with esp32 preset

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("Hello, World!");
+
+  delay(1000);
+}
